c642c90a685ac65994a9c302e56d649cab2d8d1e
Prathamesh Kasar <prathameshkasar000@gmail.com>
2024-09-22 18:19:48 +0530

    Updated MainPage.xaml.cs

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,24 +1,35 @@
-﻿namespace MPM_App
+﻿using MPM_App.Models;
+using System.Collections.ObjectModel;
+
+namespace MPM_App
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        public ObservableCollection<Process> Processes { get; set; }
 
         public MainPage()
         {
             InitializeComponent();
+            Processes = new ObservableCollection<Process>
+            {
+                new Process { PID = "1", Name = "Process 1", MemoryUsage = 100, CPUUsage = 10 },
+                new Process { PID = "2", Name = "Process 2", MemoryUsage = 200, CPUUsage = 20 },
+                new Process { PID = "3", Name = "Process 3", MemoryUsage = 150, CPUUsage = 15 }
+            };
+
+            processListView.ItemsSource = Processes;
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
-            count++;
+            var button = sender as Button;
+            var process = button.BindingContext as Process;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            if (process != null)
+            {
+                Processes.Remove(process);
+                // Logic to stop the process would go here.
+            }
         }
     }
 
