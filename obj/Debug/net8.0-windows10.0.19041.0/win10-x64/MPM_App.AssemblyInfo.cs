3c43d3c72333dfcc5071791077ec44752b254569
Prathamesh Kasar <prathameshkasar000@gmail.com>
2024-09-22 18:51:06 +0530

    Modified obj/Debug/ directory files.

--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MPM_App.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MPM_App.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MPM_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+1358f8f396be739a6536202a96581a39ea1ffde6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+35dd18449b7e469a82ecaa076fb87fc66f884875")]
 [assembly: System.Reflection.AssemblyProductAttribute("MPM_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MPM_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
