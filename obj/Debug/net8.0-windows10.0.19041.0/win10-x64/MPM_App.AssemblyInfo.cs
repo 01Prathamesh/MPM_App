cd6a80f9da59ca292460f9bb2644aa7c270f0d87
Prathamesh Kasar <prathameshkasar000@gmail.com>
2024-09-23 17:34:18 +0530

    Modified obj/Debug/

--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MPM_App.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MPM_App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MPM_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c43d3c72333dfcc5071791077ec44752b254569")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0afef53cc9a7d32320c1b67c416a52a82c60c890")]
 [assembly: System.Reflection.AssemblyProductAttribute("MPM_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MPM_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
